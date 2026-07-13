9794d57278cb83a4970b70952ece892704a27fdb
skrabytomo <skrabytomo@gmail.com>
2026-07-13 18:49:38 +0000

    Make run.sh self-configure the build dir if missing (survives rm -rf build)

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 set -e
 git pull
+# Configure the build dir if it doesn't exist yet (e.g. after rm -rf build)
+if [ ! -d build ]; then
+    cmake -B build -G Ninja
+fi
 cmake --build build -j4
 exec build/bin/unnamed_strategy
